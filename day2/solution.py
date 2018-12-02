9bbaa15f41f05204371bc220b4b207783da233a3
Rolf van Kleef <rolf@vankleef.me>
2018-12-02 11:33:51 +0100

    Fixed error in day2part2 solution.

--- day2/solution.py
+++ day2/solution.py
@@ -41,16 +41,7 @@ class Solution(Day):
         return counts2 * counts3
 
     def part2(self):
-        """
-        I found out that this program is able to produce multiple solutions for
-        my given puzzle input. The one it currently returns first is different
-        from the one I entered, so either I am making a mistake, or there are
-        just multiple possible answers.
-
-        :return: A solution
-        """
         for a, b in itertools.combinations(map(str.strip, self.data.splitlines()), 2):
-            set_a = frozenset(a)
-            intersect = [x for x in b if x in set_a]
+            intersect = [x for i, x in enumerate(b) if x == a[i]]
             if len(a) - len(intersect) == 1:
                 return "".join(intersect)
